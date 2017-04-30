b77a11b92cd17f90a37cc42e013ef4ed71533044
tony <172993974@qq.com>
2017-04-30 17:58:49 +0800

    '修改cdn地址'

--- webpack.config.js
+++ webpack.config.js
@@ -42,10 +42,28 @@ module.exports = {
             exclude: /(node_modules)/,
             loader: 'babel-loader'
         }, {
-            test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/i,
-            loader: [
-                'url-loader?limit=8192&name=img/[name]-[hash:5].[ext]'
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+
+                        loader: 'url-loader',
+                        options: { limit: 10000 }
+                    },
+                    {
+                      loader: "image-webpack-loader",
+                      options: {
+                        mozjpeg: { progressive: true, quality: 65 },
+                        optipng: { enabled: false },
+                        pngquant: { quality: [0.65, 0.9], speed: 4 },
+                        gifsicle: { interlaced: false },
+                        webp: { quality: 75 }
+                      }
+                    }
             ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf|mp3)$/,
+          use: ["file-loader"]
         }]
     },
     plugins: [
