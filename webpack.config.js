3cc4fe40c86d52f5160640976c5cb68120516764
tony <tony@futunn.com>
2019-09-13 20:04:22 +0800

    简历初始化

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,34 @@
-module.exports = {
+var webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+module.exports = {
+    entry: './js/index.js',
+    devServer: { //DevServer相关的配置
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8888,
+        inline: true,
+        open: true,
+        overlay: true,
+        hot: true,
+        publicPath: "/"
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                loader: 'css-loader?minimize',
+            }]
+        }, {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
