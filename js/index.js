7ba40f8184e50e1bf41d9062c487b12d7ea78e69
tony <tony@futunn.com>
2019-09-14 20:48:02 +0800

    完善简历

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 import 'swiper/dist/css/swiper.min.css'
 import '../style/index.css'
 import Swiper from 'swiper'
+import '../style/fonts/iconfont.css'
+
+var style_list = [
+    'icon-zhuye',
+    'icon-jinengzhangwo',
+    'icon-gongzuojingyan',
+    'icon-zuopinzhanshi',
+    'icon-lianxifangshi'
+];
 
 new Swiper('.swiper-container', {
     direction: 'vertical',
@@ -9,22 +18,43 @@ new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        renderBullet: function (index, className) {
+            var text = '';
+            switch (index) {
+                case 0:
+                    text = '基本信息';
+                    break;
+                case 1:
+                    text = '专业技能';
+                    break;
+                case 2:
+                    text = '工作经验';
+                    break;
+                case 3:
+                    text = '个人作品';
+                    break;
+                case 4:
+                    text = '联系方式';
+                    break;
+            }
+            return '<div class="' + className + '"><i class=" icon iconfont ' + style_list[index] + '"></i><span class="msg">' + text + '</span></div>';
+        }
     }
 });
 
-new Swiper('#work', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-});
+// new Swiper('#work', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: 'auto',
+//     coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 var webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
     entry: './js/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/'
+    },
     devServer: { //DevServer相关的配置
         compress: true,
         port: 8888,
@@ -35,10 +42,27 @@ module.exports = {
             test: /\.js$/,
             exclude: /(node_modules)/,
             loader: 'babel-loader'
+        }, {
+            test: /\.(woff|svg|eot|ttf)\??.*$/,
+            loader: 'url-loader?name=fonts/[name].[md5:hash:hex:7].[ext]'
+        }, {
+            test: /\.jpg|\.png|\.jpeg$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: './img',
+                    publicPath: '/img'
+                }
+            }]
         }]
     },
     plugins: [
         new MiniCssExtractPlugin(),
+        new CopyWebpackPlugin([{
+            from: __dirname + "/img",
+            to: __dirname + "/dist/img"
+        }]),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './index.html'
