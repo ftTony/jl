ce3c151c84e24ea265194b9d4a7f13eae4ebb925
tony <tony@futunn.com>
2019-09-14 12:04:48 +0800

    完整个人资料

--- js/index.js
+++ js/index.js
@@ -4,8 +4,27 @@ import Swiper from 'swiper'
 
 new Swiper('.swiper-container', {
     direction: 'vertical',
+    slidesPerView: 1,
+    mousewheel: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    }
+});
+
+new Swiper('#work', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
     },
 });
